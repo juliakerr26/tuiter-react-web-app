bd9ec219ed6c9fb0f8076b513506d4996f5c8041
Julia Kerr <juliakerr26@gmail.com>
2023-03-28 14:08:41 -0400

    like onClic fix

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -6,35 +6,59 @@ const NavigationSidebar = ({active = 'explore'}) => {
             <a className="list-group-item">Tuiter</a>
             <a className={`list-group-item list-group-item-action
                     ${active === 'home'?'active':''}`} href="/tuiter/home">
-                Home
+                <div class="row">
+                    <div class="col-2"><i class="fas fa-home"></i></div>
+                    <div class="col-10 d-none d-xl-block">Home</div>
+                </div>
             </a>
             <a className={`list-group-item list-group-item-action
                     ${active === 'explore'?'active':''}`} href="/tuiter/explore">
-                Explore
+                <div class="row">
+                    <div class="col-2"><i class="fa fa-solid fa-hashtag"></i></div>
+                    <div class="col-10 d-none d-xl-block">Explore</div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-                Notifications
+                <div class="row">
+                    <div class="col-2"><i class="fa fa-solid fa-bell"></i></div>
+                    <div class="col-10 d-none d-xl-block">Notifications</div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-                Messages
+                <div class="row">
+                    <div class="col-2"><i class="fa fa-solid fa-envelope"></i></div>
+                    <div class="col-10 d-none d-xl-block">Messages</div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-                Bookmarks
+                <div class="row">
+                    <div class="col-2"><i class="fa fa-solid fa-bookmark"></i></div>
+                    <div class="col-10 d-none d-xl-block">Bookmarks</div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-                Lists
+                <div class="row">
+                    <div class="col-2"><i class="fa fa-solid fa-list"></i></div>
+                    <div class="col-10 d-none d-xl-block">Lists</div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-                Profile
+                <div class="row">
+                    <div class="col-2"><i class="fa fa-solid fa-user"></i></div>
+                    <div class="col-10 d-none d-xl-block">Profile</div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-                More
+                <div class="row">
+                    <div class="col-2"><i class="fas fa-ellipsis-h"></i></div>
+                    <div class="col-10 d-none d-xl-block">More</div>
+                </div>
             </a>
         </div>
     );
--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,50 +1,61 @@
-import React from "react";
-import {useDispatch} from "react-redux";
-import {updateLikes} from "./tuits-reducer";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { updateLikes } from './tuits-reducer';
 import { updateTuitThunk } from '../../services/tuits-thunks';
 
-const TuitStats = (
-    {
-        post = {
-            "_id": 1,
-            "topic": "Space",
-            "userName": "SpaceX",
-            "time": "2h",
-            "title": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
-            "image": "tesla.jpeg",
-            "liked": true,
-            "replies": 123,
-            "retuits": 456,
-            "likes": 321,
-            "handle": "@spacex",
-            "tuit": "tuit content here"
-        }
-    }
-) => {
-    const dispatch = useDispatch();
-    const updateLikesHandler = (id) => {
-        dispatch(updateLikes(id));
-    }
-
-    return(
-
-        <div className="row">
-            <div className="col-3">
-                <i className="fa-regular fa-comment"></i> {post.replies}
-            </div>
-            <div className="col-3">
-                <i className="fa-solid fa-retweet"></i> {post.retuits}
-            </div>
-            <div className="col-3" onClick={() => updateLikesHandler(post._id)}>
-                {post.liked ?
-                <i className="fa-solid fa-heart" style={{color: "#ff2600",}}></i> :
-                <i className="fa-solid fa-heart"></i>} {post.likes}
-            </div>
-            <div className="col-3">
-                <i className="fa-solid fa-share-nodes"></i>
-            </div>
-        </div>
+const TuitStats = ({
+  post = {
+    _id: 1,
+    topic: 'Space',
+    userName: 'SpaceX',
+    time: '2h',
+    title: "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
+    image: 'tesla.jpeg',
+    liked: true,
+    replies: 123,
+    retuits: 456,
+    likes: 321,
+    handle: '@spacex',
+    tuit: 'tuit content here',
+  },
+}) => {
+  const dispatch = useDispatch();
+  const updateLikesHandler = (updateLiked, updateLikes) => {
+    dispatch(
+      updateTuitThunk({
+        ...post,
+        likes: post.likes + updateLikes,
+        liked: updateLiked,
+      })
     );
+  };
+
+  return (
+    <div className="row">
+      <div className="col-3">
+        <i className="fa-regular fa-comment"></i> {post.replies}
+      </div>
+      <div className="col-3">
+        <i className="fa-solid fa-retweet"></i> {post.retuits}
+      </div>
+      <div
+        className="col-3"
+        onClick={() => {
+          post.liked ? updateLikesHandler(false, -1) : updateLikesHandler(true, 1);
+        }}
+      >
+        {post.liked ? (
+          <i className="fa-solid fa-heart" style={{ color: '#ff2600' }}></i>
+        ) : (
+          <i className="fa-solid fa-heart"></i>
+        )}{' '}
+        {post.likes}
+      </div>
+      <div className="col-3">
+        <i className="fa-solid fa-share-nodes"></i>
+      </div>
+    </div>
+  );
 };
 
 export default TuitStats;
